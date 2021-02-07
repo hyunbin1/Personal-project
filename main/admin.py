b7b7a8bd77bca3f1f709e26ee60a88237349e2f1
hyunbin1 <kimgusqls1@gmail.com>
2021-02-07 10:39:29 +0900

    fix models to print data object

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Question
 
+
+# Searching data at django admin page
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
--- main/models.py
+++ main/models.py
@@ -7,6 +7,10 @@ class Question(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
 
+    # 데이터 객체 id 번호 대신 이름으로 호출하기
+    def __str__(self):
+        return self.subject
+
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
